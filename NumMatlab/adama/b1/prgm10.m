5bd2de421b6bcef88ceefa1569d48fa5564de694
Sandy Herho <sandy.herho@ronininstitute.org>
2023-04-21 00:42:01 -0400

    nambah

--- NumMatlab/adama/b1/prgm10.m
+++ NumMatlab/adama/b1/prgm10.m
@@ -1,2 +1,16 @@
 clear all; close all; clc
-biot(0.02, 200, 120)
+bi = biot(0.0786, 58, 205);
+display(bi);
+
+if bi < 0.1
+    Tf = input("Tf: \n");
+    Tfl = input("Tfl: \n");
+    To = input("To: \n");
+    r = .0786; h = 60; rho = 2700; cp = 900;
+    T = log((Tf - Tfl) / (To -Tfl));
+    fra = -((h*3) / (rho * cp *r));
+    time = (T/fra);
+    display(time)
+else
+    display("use Heislers chart to find the solution.")
+end

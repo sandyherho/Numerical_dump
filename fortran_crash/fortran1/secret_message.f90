ff155dcf736a0d4036e7675f8ec7e800b9becf78
Sandy Herho <sandy.herho@ronininstitute.org>
2023-05-29 00:27:22 -0400

    nambah

---
+++ fortran_crash/fortran1/secret_message.f90
@@ -0,0 +1,7 @@
+program secret_message
+    implicit none
+ 
+    print * , &
+        char (87) // char (69) // char (76) // char (67) &
+        // char (79) // char (77) // char (69)
+end program secret_message

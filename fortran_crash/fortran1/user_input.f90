40068adb0a00ee0261d960bcdda0f2dcadcde91f
Sandy Herho <sandy.herho@ronininstitute.org>
2023-06-29 23:37:30 -0400

    nambah

--- fortran_crash/fortran1/user_input.f90
+++ fortran_crash/fortran1/user_input.f90
@@ -4,9 +4,18 @@ program user_input
 	character(len=20) :: hometown
 	integer :: age
 	character(len=20) :: name
+	integer :: status
 
-	print *, "Can you tell me your name, age, and where you lived?"
-	read(*,*) name, age, hometown
+	name ="UNKNOWN"
+	age = 0
+	hometown = "UNKNOWN"
+
+	do
+		print *, "Can you tell me your name, age, and where you lived?"
+		read(*, *, iostat=status) name, age, hometown
+		if (status == 0) exit
+		print *, "Sorry, I didn't understand that."
+	end do
 
 	print *, "Hi, " // trim(name) // "!"
 	print *, "You look great for ", age
